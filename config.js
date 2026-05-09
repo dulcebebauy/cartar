07099f113fc63591cf07ebed9dfd916d1191b84c
dulcebebauy <dulcebeba.uy@gmail.com>
2026-05-09 20:45:54 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,2 @@
+const API_DESA = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRuSw5jl4qGH2GTE_zvE_nDHqKULbK5XH0Wl0Dpl4OoJQNSDYs7DHX5AdNTmhl8ukGuThXrzKddE58m/pub?output=csv";
+const API_PROD = "";
