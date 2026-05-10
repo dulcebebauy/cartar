e716c9352d4dcee87d8dab40633ff1f23b7ee3b2
dulcebebauy <dulcebeba.uy@gmail.com>
2026-05-10 20:21:53 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 const API_DESA = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRuSw5jl4qGH2GTE_zvE_nDHqKULbK5XH0Wl0Dpl4OoJQNSDYs7DHX5AdNTmhl8ukGuThXrzKddE58m/pub?output=csv";
-const API_PROD = "";
+const API_PROD = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQfvjGjPA_YlkgQs2uyZqp3jHWE0FpPVlSm4iIELBFbP0ycryTnNBLF5yluwnNBhzJEJMTeAEYraAQ0/pub?output=csv";
